da036573c332cc49098a2b9a2989abca764e69e7
Alexandra Krause <alexandra.krause2@gmail.com>
2019-02-02 18:21:54 +0100

    Added random number generator
    This script simulates a quantum random number generator by projecting qubits to the Hadamard basis and then measure each of it in the computational basis. This gives you a bit string of random numbers {0,1}^n. Note that the probability should converge to 0.5 for an actual quantum system. As I do not know the quality of Googles simulator used here and do not know benchmarking stats for it take with a grain of salt. You can try to obtain stats though by running the simulation several times and then see the stats in a histogram. I will add this function later but first wanted to make sure that the script runs for small numbers of qubits even on outdated hardware.

---
+++ random_bit_generator.py
@@ -0,0 +1,29 @@
+#initialize a superposition as 1/sqrt(2) (0+1) and then measure in computational basis
+#this should deliver a random bit
+
+import cirq
+
+#final number of bits divided by 2
+num_bits = 3
+
+#initialize qubits
+qubits = [cirq.GridQubit(i, j) for i in range(num_bits) for j in range(2)]
+
+#superpose 2 qubits for every subgrid
+
+superposition_circuit = cirq.Circuit()
+superposition_circuit.append(cirq.H(q) for q in qubits)
+
+#measure every qubit in computational basis
+
+superposition_circuit.append(cirq.measure(*qubits, key='z'))
+
+print(qubits)
+print(superposition_circuit)
+
+
+#run simulation of circuit
+simulator = cirq.google.XmonSimulator()
+
+results = simulator.run(superposition_circuit, repetitions=1)
+print(results)
